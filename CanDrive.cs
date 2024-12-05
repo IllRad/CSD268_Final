c9258ab5f8bb7175abfbd223d3905ebac142bfd3
IllRad <radchukillya6@gmail.com>
2024-12-05 13:54:13 -0800

    Create CanDrive.cs

---
+++ CanDrive.cs
@@ -0,0 +1,9 @@
+// Code file for CanDrive() function using C#:
+
+
+// Coppied code from assignment options
+public bool CanDrive(int age) {
+
+   const int drivingAge = 16; return age >= drivingAge;
+
+}
